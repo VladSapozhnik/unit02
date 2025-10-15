4842528adf3b8c4066757bd8b9291fd1d4949584
VladSapozhnik <vladbars2@gmail.com>
2025-10-15 20:33:07 +0300

    fix validatte wbsiteURL length

--- dist/validators/blogs/base-blog.validator.js
+++ dist/validators/blogs/base-blog.validator.js
@@ -6,5 +6,5 @@ const express_validator_1 = require("express-validator");
 exports.baseBlogValidator = [
     (0, express_validator_1.body)('name').isString().withMessage('Name must be a string').trim().isLength({ min: 2, max: 15 }).withMessage('Name length must be between 2 and 15'),
     (0, express_validator_1.body)('description').isString().withMessage('Description must be a string').trim().isLength({ min: 2, max: 500 }).withMessage('Description length must be between 2 and 500'),
-    (0, express_validator_1.body)('websiteUrl').isString().withMessage('WebsiteUrl must be a string').matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('WebsiteUrl must be a valid URL starting with https://'),
+    (0, express_validator_1.body)('websiteUrl').isString().withMessage('WebsiteUrl must be a string').trim().isLength({ min: 2, max: 100 }).withMessage('websiteUrl length must be between 2 and 100').matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('WebsiteUrl must be a valid URL starting with https://'),
 ];
--- src/validators/blogs/base-blog.validator.ts
+++ src/validators/blogs/base-blog.validator.ts
@@ -4,5 +4,5 @@ import { body } from 'express-validator';
 export const baseBlogValidator = [
     body('name').isString().withMessage('Name must be a string').trim().isLength({ min: 2, max: 15 }).withMessage('Name length must be between 2 and 15'),
     body('description').isString().withMessage('Description must be a string').trim().isLength({ min: 2, max: 500 }).withMessage('Description length must be between 2 and 500'),
-    body('websiteUrl').isString().withMessage('WebsiteUrl must be a string').matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('WebsiteUrl must be a valid URL starting with https://'),
+    body('websiteUrl').isString().withMessage('WebsiteUrl must be a string').trim().isLength({ min: 2, max: 100 }).withMessage('websiteUrl length must be between 2 and 100').matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('WebsiteUrl must be a valid URL starting with https://'),
 ]
