21b45ea1baf8356d26ef9a1ae4d9d79f929f71be
VladSapozhnik <vladbars2@gmail.com>
2025-10-22 12:05:43 +0300

    connect mongodb cloud

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,6 +1,13 @@
 export const settings = {
   PORT: Number(process.env.PORT) || 5000,
-  DB_URL: process.env.MANGO_URI || 'mongodb://127.0.0.1:27017/blogs',
-  DB_URL_TESTING: 'mongodb://127.0.0.1:27017/testing',
-  DB_NAME: process.env.DB_NAME || 'blogs',
+  // DB_URL: process.env.MANGO_URI || 'mongodb://127.0.0.1:27017/blogs',
+  DB_URL:
+    process.env.MANGO_URI ||
+    'mongodb+srv://vladbars2:vlad34299@cluster0.jgd7edn.mongodb.net/',
+
+  // DB_URL_TESTING: 'mongodb://127.0.0.1:27017/testing',
+  DB_URL_TESTING:
+    'mongodb+srv://vladbars2:vlad34299@cluster0.jgd7edn.mongodb.net/testing',
+
+  DB_NAME: process.env.DB_NAME || 'blog-it-incubator',
 };
