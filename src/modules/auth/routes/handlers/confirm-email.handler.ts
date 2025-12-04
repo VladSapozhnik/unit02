3b074f10203cdb6d5e43216d3f87fac26c3988ef
VladSapozhnik <vladbars2@gmail.com>
2025-12-04 13:33:46 +0200

    fix confirm email

--- src/modules/auth/routes/handlers/confirm-email.handler.ts
+++ src/modules/auth/routes/handlers/confirm-email.handler.ts
@@ -3,12 +3,18 @@ import { authService } from '../../application/auth.service';
 import { RequestWithBody } from '../../../../core/types/request.type';
 import { HTTP_STATUS } from '../../../../core/enums/http-status.enum';
 import { ConfirmEmailDto } from '../../dto/confirm-email.dto';
+import { ResultStatus } from '../../../../core/enums/result-status.enum';
 
 export const confirmEmailHandler = async (
   req: RequestWithBody<ConfirmEmailDto>,
   res: Response,
 ) => {
-  await authService.confirmEmail(req.body.code);
+  const { status } = await authService.confirmEmail(req.body.code);
+
+  if (status === ResultStatus.BadRequest) {
+    res.sendStatus(400);
+    return;
+  }
 
   res.sendStatus(HTTP_STATUS.NO_CONTENT_204);
 };
