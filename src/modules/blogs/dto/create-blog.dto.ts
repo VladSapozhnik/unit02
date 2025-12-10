15ee450f8d2454d41172ef082ca30e39a43812d6
VladSapozhnik <vladbars2@gmail.com>
2025-12-10 16:16:41 +0200

    fix blog dto and class

--- src/modules/blogs/dto/create-blog.dto.ts
+++ src/modules/blogs/dto/create-blog.dto.ts
@@ -1,8 +1,14 @@
-export type CreateBlogDto = {
+export class CreateBlogDto {
   /**
    * body create dto
    */
   name: string;
   description: string;
   websiteUrl: string;
-};
+
+  constructor(name: string, description: string, websiteUrl: string) {
+    this.name = name;
+    this.description = description;
+    this.websiteUrl = websiteUrl;
+  }
+}
--- src/modules/blogs/dto/update-blog.dto.ts
+++ src/modules/blogs/dto/update-blog.dto.ts
@@ -1,8 +1,23 @@
-export type UpdateBlogDto = {
+// export type UpdateBlogDto = {
+//   /**
+//    * body for updating successfully found dto
+//    */
+//   name: string;
+//   description: string;
+//   websiteUrl: string;
+// };
+
+export class UpdateBlogDto {
   /**
    * body for updating successfully found dto
    */
   name: string;
   description: string;
   websiteUrl: string;
-};
+
+  constructor(name: string, description: string, websiteUrl: string) {
+    this.name = name;
+    this.description = description;
+    this.websiteUrl = websiteUrl;
+  }
+}
