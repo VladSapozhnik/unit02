7a5336cab782b50cda1bfdf0b56a0855b298cc6b
VladSapozhnik <vladbars2@gmail.com>
2025-12-10 11:40:48 +0200

    fix

--- src/modules/blogs/application/blogs.service.ts
+++ src/modules/blogs/application/blogs.service.ts
@@ -1,7 +1,6 @@
 import { BlogType } from '../types/blog.type';
 import { CreateBlogDto } from '../dto/create-blog.dto';
 import { UpdateBlogDto } from '../dto/update-blog.dto';
-// import { blogsRepository } from '../repositories/blogs.repository';
 import { createdAtHelper } from '../../../core/helpers/created-at.helper';
 import { NotFoundError } from '../../../core/errors/repository-not-found.error';
 import { BadRequestError } from '../../../core/errors/bad-request.error';
@@ -51,41 +50,3 @@ export class BlogsService {
     return isDeleted;
   }
 }
-
-// export const blogsService = {
-//   async createBlog(body: CreateBlogDto): Promise<string> {
-//     const newBlog: BlogType = {
-//       ...body,
-//       createdAt: createdAtHelper(),
-//       isMembership: false,
-//     };
-//
-//     const blogId: string = await blogsRepository.createBlog(newBlog);
-//
-//     if (!blogId) {
-//       throw new BadRequestError('Failed to create blog', 'blog');
-//     }
-//
-//     return blogId;
-//   },
-//
-//   async updateBlog(id: string, body: UpdateBlogDto): Promise<boolean> {
-//     const isUpdated: boolean = await blogsRepository.updateBlog(id, body);
-//
-//     if (!isUpdated) {
-//       throw new NotFoundError('Id is not found', 'blog');
-//     }
-//
-//     return isUpdated;
-//   },
-//
-//   async removeBlogById(id: string): Promise<boolean> {
-//     const isDeleted: boolean = await blogsRepository.removeBlogById(id);
-//
-//     if (!isDeleted) {
-//       throw new NotFoundError('Id is not found', 'blog');
-//     }
-//
-//     return isDeleted;
-//   },
-// };
